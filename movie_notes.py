f20b6fced825e05f113ca9b643464968d4b5fc58
10asmock <hegemon1984@gmail.com>
2017-11-16 15:43:29 -0800

    Add files via upload

---
+++ movie_notes.py
@@ -0,0 +1,30 @@
+#### Notes #####
+    
+# The __init__ function creates things for class Movie to remember such as
+# title, storyline, poster, and trailer.
+
+# The keyword self is the object or instance being created.
+# In this case, self is evil_within_2 
+
+# Remember, parameters and arguments are not the same:
+        # A parameter = the declaration of a function (ie. def sum(a,b)).
+        # An argument = the actual value passed onto a function
+        #(ie. def __init__(self, game_title, game_summary))
+
+# An instance variable are unique variables attached to classes:
+# For example, "title = 'Evil Within 2'"
+
+# An instance method is a function defined inside a class and associated within
+# an instance. For example, "show_trailer()".
+
+# Despite what I believed, arrays and lists are not the same thing in Python. 99%
+# of the time, I will use lists because they're flexible.
+
+# Constant variables like "VALID_RATINGS" are all capitalized.
+
+# The __doc__ keyword gives information on the class you're using, for example
+# using "turtle.Turtle.__doc__" explains what the class Turtle does in Python.
+
+# For the media.py code, we're using triple quotes(""") below class Movie() to
+# cocaconate it to __doc__ so when we type out "media.Movie.__doc__", we get
+# documentation as to what media.py does.
